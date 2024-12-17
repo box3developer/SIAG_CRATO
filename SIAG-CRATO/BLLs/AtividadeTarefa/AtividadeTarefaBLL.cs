c044e33ac2fcd608a155b2ad44ff1f384fd26295
CaioFSousa <felipesousa.2243@gmail.com>
2024-12-17 08:26:46 -0300

    feat: adicionado AtividadeTarefaBLL

---
+++ SIAG-CRATO/BLLs/AtividadeTarefa/AtividadeTarefaBLL.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace SIAG_CRATO.BLLs.AtividadeTarefa;
+
+public class AtividadeTarefaBLL
+{
+    public static async Task<List<(int idTarefa, int sequencia)>> GetListAsync(int idAtividade)
+    {
+        using var conexao = new SqlConnection(Global.Conexao);
+        var tarefas = await conexao.QueryAsync<(int idTarefa, int sequencia)>(AtividadeTarefaQuery.SELECT, new { idAtividade });
+
+        return tarefas.ToList();
+    }
+
+    public static async Task<List<(int idTarefa, int sequencia)>> GetByAtividadeAsync(int idAtividade)
+    {
+        var sql = $"{AtividadeTarefaQuery.SELECT} WHERE id_atividade = @IdAtividade";
+
+        using var conexao = new SqlConnection(Global.Conexao);
+        var tarefas = await conexao.QueryAsync<(int idTarefa, int sequencia)>(sql, new { idAtividade });
+
+        return tarefas.ToList();
+    }
+}
---
+++ SIAG-CRATO/BLLs/AtividadeTarefa/AtividadeTarefaQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace SIAG_CRATO.BLLs.AtividadeTarefa;
+
+public class AtividadeTarefaQuery
+{
+    public const string SELECT = "SELECT id_tarefa, cd_sequencia FROM atividadetarefa WITH(NOLOCK)";
+}
