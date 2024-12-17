982a1ac82e84a2b28e26abe4e368035a8a8a389f
CaioFSousa <felipesousa.2243@gmail.com>
2024-12-17 08:26:59 -0300

    feat: adicionado ChamadaPendenteBLL

---
+++ SIAG-CRATO/BLLs/ChamadaPendencia/ChamadaPendenciaBLL.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace SIAG_CRATO.BLLs.ChamadaPendencia;
+
+public class ChamadaPendenciaBLL
+{
+    public static async Task<bool> SetChamadaPai(Guid idChamada, Guid idChamadaPai)
+    {
+        using var conexao = new SqlConnection(Global.Conexao);
+        var id = await conexao.ExecuteAsync(ChamadaPendenciaQuery.UPDATE_CHAMADA_PAI, new
+        {
+            idChamadaPai,
+            idChamada
+        });
+
+        return id > 0;
+    }
+
+    public static async Task<bool> SetChamadaOrigem(Guid idChamada, Guid idChamadaOrigem)
+    {
+        using var conexao = new SqlConnection(Global.Conexao);
+        var id = await conexao.ExecuteAsync(ChamadaPendenciaQuery.UPDATE_CHAMADA_ORIGEM, new
+        {
+            idChamadaOrigem,
+            idChamada
+        });
+
+        return id > 0;
+    }
+}
---
+++ SIAG-CRATO/BLLs/ChamadaPendencia/ChamadaPendenciaQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace SIAG_CRATO.BLLs.ChamadaPendencia;
+
+public class ChamadaPendenciaQuery
+{
+    public const string UPDATE_CHAMADA_PAI = @"UPDATE chamadadependencia SET id_chamada = @idChamadaPai WHERE id_chamada = @idChamada";
+    public const string UPDATE_CHAMADA_ORIGEM = @"UPDATE chamadadependencia SET id_chamadapai = @idChamadaOrigem WHERE id_chamadapai = @idChamada";
+}
