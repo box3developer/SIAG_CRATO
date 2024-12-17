b77251bf3b215181c88b4ad2223926be75cbc459
CaioFSousa <felipesousa.2243@gmail.com>
2024-12-17 08:24:39 -0300

    feat: adicionado ChamadaPendenteBLL

---
+++ SIAG-CRATO/BLLs/ChamadaTarefa/ChamadaTarefaBLL.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace SIAG_CRATO.BLLs.ChamadaTarefa;
+
+public class ChamadaTarefaBLL
+{
+    public static async Task<List<(int idTarefa, int idChamada)>> GetListAsync()
+    {
+        using var conexao = new SqlConnection(Global.Conexao);
+        var tarefas = await conexao.QueryAsync<(int idTarefa, int idChamada)>(ChamadaTarefaQuery.SELECT);
+
+        return tarefas.ToList();
+    }
+
+    public static async Task<bool> SetTarefaAsync(Guid idChamada, int idTarefa)
+    {
+        using var conexao = new SqlConnection(Global.Conexao);
+        var tarefa = await conexao.ExecuteAsync(ChamadaTarefaQuery.INSERT, new { idChamada, idTarefa });
+
+        return tarefa > 0;
+    }
+}
---
+++ SIAG-CRATO/BLLs/ChamadaTarefa/ChamadaTarefaQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace SIAG_CRATO.BLLs.ChamadaTarefa;
+
+public class ChamadaTarefaQuery
+{
+    public const string SELECT = "SELECT id_tarefa, id_chamada FROM chamadatarefa WITH(NOLOCK)";
+    public const string INSERT = "INSERT INTO chamadatarefa (id_chamada, id_tarefa) VALUES (@idChamada, @idTarefa)";
+}
