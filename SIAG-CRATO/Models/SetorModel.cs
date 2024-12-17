5e48b78636a01291e6b4242e8d57fd75777fd2d6
CaioFSousa <felipesousa.2243@gmail.com>
2024-12-17 12:11:19 -0300

    feat: adicionado SetorModel

---
+++ SIAG-CRATO/Models/SetorModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SIAG_CRATO.Models;
+
+public class SetorModel
+{
+    [Column("id_setortrabalho")]
+    public int Codigo { get; set; }
+
+    [Column("id_deposito")]
+    public int IdDeposito { get; set; }
+
+    [Column("nm_setortrabalho")]
+    public string Descricao { get; set; } = string.Empty;
+}
+
