1d5da0c88d5d0419138070460c1bc1fdf768776c
CaioFSousa <felipesousa.2243@gmail.com>
2024-12-17 11:32:43 -0300

    feat: adicioando TurnoModel

---
+++ SIAG-CRATO/BLLs/Turno/TurnoQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace SIAG_CRATO.BLLs.Turno;
+
+public class TurnoQuery
+{
+    public const string SELECT = "SELECT cd_turno, dt_inicio, dt_fim FROM turno WITH(NOLOCK)";
+}
---
+++ SIAG-CRATO/Models/TurnoModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SIAG_CRATO.Models;
+
+public class TurnoModel
+{
+    [Column("cd_turno")]
+    public string? CodTurno { get; set; }
+
+    [Column("dt_inicio")]
+    public DateTime DtInicio { get; set; }
+
+    [Column("dt_fim")]
+    public DateTime DtFim { get; set; }
+}
