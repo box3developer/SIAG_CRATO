3524c5d3b4c337ec190aa6eabdaf458d61d9f4aa
CaioFSousa <felipesousa.2243@gmail.com>
2024-12-17 12:11:36 -0300

    feat: adicionado AtividadeRotinaModel

---
+++ SIAG-CRATO/BLLs/AtividadeRotina/AtividadeRotinaQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace SIAG_CRATO.BLLs.AtividadeRotina;
+
+public class AtividadeRotinaQuery
+{
+    public const string SELECT = "SELECT id_atividadeRotina, nm_AtividadeRotina, nm_procedure, fg_tipo FROM atividaderotina WITH(NOLOCK)";
+}
--- SIAG-CRATO/Models/AtividadeRotinaModel.cs
+++ SIAG-CRATO/Models/AtividadeRotinaModel.cs
@@ -1,10 +1,18 @@
-﻿using SIAG_CRATO.Data;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using SIAG_CRATO.Data;
 
 namespace SIAG_CRATO.Models;
 public class AtividadeRotinaModel
 {
+    [Column("id_atividadeRotina")]
     public int Codigo { get; set; }
+
+    [Column("nm_AtividadeRotina")]
     public string Nome { get; set; } = string.Empty;
+
+    [Column("nm_procedure")]
     public string Procedure { get; set; } = string.Empty;
+
+    [Column("fg_tipo")]
     public TipoRotina Tipo { get; set; }
 }
