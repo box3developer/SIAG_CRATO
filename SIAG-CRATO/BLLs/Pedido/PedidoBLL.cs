f412a12d75af713cb4b1b189dd7ec0a3b44a65da
CaioFSousa <felipesousa.2243@gmail.com>
2024-12-20 17:28:52 -0300

    feat: adicionado modelo Pedido

---
+++ SIAG-CRATO/BLLs/Pedido/PedidoBLL.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SIAG_CRATO.Models;
+
+namespace SIAG_CRATO.BLLs.Pedido;
+
+public class PedidoBLL
+{
+    public static async Task<PedidoModel?> GetById(string id)
+    {
+        var sql = $"{PedidoQuery.SELECT} WHERE id_pedido = @id";
+
+        using var conexao = new SqlConnection(Global.Conexao);
+        var pedido = await conexao.QueryFirstOrDefaultAsync<PedidoModel>(sql, new { id });
+
+        return pedido;
+    }
+}
---
+++ SIAG-CRATO/Models/PedidoModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SIAG_CRATO.Models;
+
+public class PedidoModel
+{
+    [Column("id_pedido")]
+    public string? IdPedido { get; set; }
+
+    [Column("cd_lote")]
+    public string? CodigoLote { get; set; }
+}
