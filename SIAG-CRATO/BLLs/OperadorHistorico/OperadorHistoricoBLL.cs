597f95784c8627eabf21588f3a931d3dbfd8c663
CaioFSousa <felipesousa.2243@gmail.com>
2025-01-15 10:54:33 -0300

    fix: ajuste no insert do operador historico

--- SIAG-CRATO/BLLs/OperadorHistorico/OperadorHistoricoBLL.cs
+++ SIAG-CRATO/BLLs/OperadorHistorico/OperadorHistoricoBLL.cs
@@ -8,10 +8,13 @@ public class OperadorHistoricoBLL
     public static async Task<bool> SetHistorico(int id_operador, int id_equipamento)
     {
         using var conexao = new SqlConnection(Global.Conexao);
+        var result = await conexao.ExecuteAsync(OperadorHistoricoQuery.INSERT, new
+        {
+            id_operador,
+            evento = 1,
+            id_equipamento
+        });
 
-        var sql = $@"{OperadorHistoricoQuery.INSERT}";
-
-        var result = await conexao.ExecuteAsync(sql, new { id_operador, evento = 1, id_equipamento });
         return result > 0;
     }
 }
--- SIAG-CRATO/BLLs/OperadorHistorico/OperadorHistoricoQuery.cs
+++ SIAG-CRATO/BLLs/OperadorHistorico/OperadorHistoricoQuery.cs
@@ -1,10 +1,7 @@
-﻿namespace SIAG_CRATO.BLLs.OperadorHistorico
+﻿namespace SIAG_CRATO.BLLs.OperadorHistorico;
+
+public class OperadorHistoricoQuery
 {
-    public class OperadorHistoricoQuery
-    {
-        public const string INSERT = @"INSERT INTO operadorhistorico 
-                                        (id_operador,id_equipamento,cd_evento,dt_evento) 
-		                                VALUES (@id_operador,@id_equipamento,@evento ,getdate())
-";
-    }
+    public const string INSERT = @"INSERT INTO operadorhistorico (id_operador, id_equipamento, cd_evento, dt_evento) 
+		                               VALUES (@id_operador, @id_equipamento, @evento, GETDATE())";
 }
