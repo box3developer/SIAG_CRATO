af4fa279a8c3aeef005cf36e6f903be3972d8549
CaioFSousa <felipesousa.2243@gmail.com>
2025-01-15 10:57:32 -0300

    refactor: ajuste nas propriedades global

--- SIAG-CRATO/Global.cs
+++ SIAG-CRATO/Global.cs
@@ -2,7 +2,6 @@
 
 public class Global
 {
-    //public static readonly string Conexao = "server=;user id=;password=;database=;Timeout=300;TrustServerCertificate=True";
-    public static string Conexao = "";
-    public static string NodeRedAPI = "";
+    public static string Conexao { get; set; } = string.Empty;
+    public static string NodeRedAPI { get; set; } = string.Empty;
 }
