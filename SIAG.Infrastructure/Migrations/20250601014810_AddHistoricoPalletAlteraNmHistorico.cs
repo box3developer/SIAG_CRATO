e50fd02060f44f57e928284578282aae8cff1e02
cristhyano <cristhyanocomthy@gmail.com>
2025-05-31 22:49:58 -0300

    Update NmHistorico type and connection string
    The `NmHistorico` property in `HistoricoPallet.cs` was changed from `int` to `string` with a default value of `string.Empty`. A corresponding database migration was added to update the `nm_historico` column in the `historicopallet` table to `nvarchar(max)` and non-nullable. The `SiagDbContextModelSnapshot` was updated accordingly.
    
    Additionally, the connection string for `SqlServerSIAGCrato` in `appsettings.json` was updated to change the server address from `192.168.0.6` to `192.168.0.8`.

---
+++ SIAG.Infrastructure/Migrations/20250601014810_AddHistoricoPalletAlteraNmHistorico.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIAG.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHistoricoPalletAlteraNmHistorico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "nm_historico",
+                table: "historicopallet",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "nm_historico",
+                table: "historicopallet",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
