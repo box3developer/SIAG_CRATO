2b01b6bccc3eccdc7bd1a0d24c79ad22581b1186
cristhyano <cristhyanocomthy@gmail.com>
2025-06-01 06:00:56 -0300

    Make several properties nullable in domain models
    Changed the following properties to nullable types:
    - `IdAtividade`, `IdPalletOrigem`, and `IdPalletDestino` from `int` to `int?`.
    - `IdAreaArmazenagemOrigem` and `IdAreaArmazenagemDestino` from `long` to `long?`.
    - `Priorizar` from `bool` to `bool?`, retaining its default value of `false`.

--- SIAG-CRATO/Services/CriarChamadaDTO.cs
+++ SIAG-CRATO/Services/CriarChamadaDTO.cs
@@ -2,16 +2,16 @@
 {
     public class CriarChamadaDTO
     {
-        public int IdAtividade { get; set; }
+        public int? IdAtividade { get; set; }
 
-        public int IdPalletOrigem { get; set; }
+        public int? IdPalletOrigem { get; set; }
 
-        public long IdAreaArmazenagemOrigem { get; set; }
+        public long? IdAreaArmazenagemOrigem { get; set; }
 
-        public int IdPalletDestino { get; set; }
+        public int? IdPalletDestino { get; set; }
 
-        public long IdAreaArmazenagemDestino { get; set; }
+        public long? IdAreaArmazenagemDestino { get; set; }
 
-        public bool Priorizar { get; set; } = false;
+        public bool? Priorizar { get; set; } = false;
     }
 }
